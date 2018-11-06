819d4e8ebdf7a6091e2167ccb2a1f76e0f154839
satriaramir <satriaramir@gmail.com>
2018-11-06 13:26:06 +0700

    splashsr]creen

--- app/src/main/java/com/example/root/shafood/SplashScreen.java
+++ app/src/main/java/com/example/root/shafood/SplashScreen.java
@@ -4,23 +4,40 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    protected TextView tv;
+    private ImageView iv;
     private static int SPLAS_TIMR_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread() {
             public void run() {
-                Intent SplashIntent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(SplashIntent);
-                finish();
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
             }
-        }, SPLAS_TIMR_OUT);
+        };
+        timer.start();
+
     }
 }
