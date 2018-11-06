17031ff560b5e6e147e735fbbf6747eff477cec3
kazehiro <adrianhadi@outlook.com>
2018-11-06 10:40:53 +0700

    pertama

---
+++ app/src/main/java/com/example/root/shafood/DonaturActivity.java
@@ -0,0 +1,122 @@
+package com.example.root.shafood;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class DonaturActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private ImageView imageView;
+
+
+    private static final String TAG = "AddToDatabase";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donatur);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://shafood93.appspot.com");
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+        FirebaseUser user = mAuth.getCurrentUser();
+        final String userID = user.getUid();
+        storageRef.child("Donatur/FotoProfil/"+userID).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                System.out.println(uri);
+                Glide.with(getApplicationContext()).load(uri).into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+    }
+
+    public void find(View view) {
+        Log.d(TAG, "onClick: Attempting to add object to database.");
+        FirebaseUser user = mAuth.getCurrentUser();
+        String userID = user.getUid();
+        Long tsLong = System.currentTimeMillis() / 1000;
+        String ts = tsLong.toString();
+        Transaksi newTransaksi = new Transaksi(userID + ts, userID, userID + ts, userID + ts, userID + ts, userID + ts, userID + ts, userID + ts, userID + ts, 1);
+        myRef.child("SHAFOOD").child("TRANSAKSI").child(userID + ts).setValue(newTransaksi);
+        toastMessage("Adding User to database...");
+    }
+
+    public void search(View view) {
+        Intent search = new Intent(DonaturActivity.this, ShowPenerima.class);
+        startActivity(search);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
