e0d053a579f4a7eaada0bbf93b076ebd07b55ad6
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2018-11-09 10:03:24 +0700

    QrCode

--- app/src/main/java/com/example/root/shafood/Penerima_Main.java
+++ app/src/main/java/com/example/root/shafood/Penerima_Main.java
@@ -1,13 +1,29 @@
 package com.example.root.shafood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Penerima_Main extends AppCompatActivity {
+
+    private Button BtnQrcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penerima__main);
+
+    BtnQrcode = (Button) findViewById(R.id.BtnQrcode);
+
+
+    BtnQrcode.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        Intent mIntent = new Intent(Penerima_Main.this, Penerima_Verifikasi.class);
+        startActivity(mIntent);
+        }
+    });
     }
 }
---
+++ app/src/main/java/com/example/root/shafood/Penerima_Verifikasi.java
@@ -0,0 +1,160 @@
+package com.example.root.shafood;
+
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Penerima_Verifikasi extends AppCompatActivity {
+
+    EditText text;
+    Button gen_btn;
+    ImageView image;
+    String text2Qr;
+
+    private static final String TAG = "ViewDatabase";
+
+    //add Firebase Database stuff
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penerima__verifikasi);
+
+        //declare the database reference object. This is what we use to access the database.
+        //NOTE: Unless you are signed in, this will not be useable.
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference().child("SHAFOOD").child("USER").child("PENERIMA");
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+            }
+        };
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData((Map<String, Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        text = (EditText) findViewById(R.id.text);
+        gen_btn = (Button) findViewById(R.id.gen_btn);
+        image = (ImageView) findViewById(R.id.image);
+        gen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text2Qr = "Dwiky Tolol";
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+                }
+                catch (WriterException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void showData(Map<String, Object> dataSnapshot) {
+        final ArrayList<String> Id_donatur = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_donatur = (Map) entry.getValue();
+            Id_donatur.add((String) id_donatur.get("id_donatur"));
+        }
+        ArrayList<String> Id_penerima = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_penerima = (Map) entry.getValue();
+            Id_penerima.add((String) id_penerima.get("id_penerima"));
+        }
+        ArrayList<String> Id_transaksi = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_transaksi = (Map) entry.getValue();
+            Id_transaksi.add((String) id_transaksi.get("id_transaksi"));
+        }
+        ArrayList<String> Id_kurir = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_kurir = (Map) entry.getValue();
+            Id_kurir.add((String) id_kurir.get("id_kurir"));
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    /**
+     * customizable toast
+     *
+     * @param message
+     */
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
