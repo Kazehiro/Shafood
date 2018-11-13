ff1fcba887b653c18130737c2c02e7e5455982d0
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2018-11-13 14:53:34 +0700

    Kurir_verifikasi
    (cherry picked from commit 9d6fcaa)

--- app/src/main/java/com/example/root/shafood/ShowPenerima.java
+++ app/src/main/java/com/example/root/shafood/ShowPenerima.java
@@ -1,6 +1,11 @@
 package com.example.root.shafood;
 
+import android.app.Dialog;
+import android.app.Notification;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -10,9 +15,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +32,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.lang.ref.ReferenceQueue;
 import java.util.ArrayList;
@@ -38,13 +52,26 @@ public class ShowPenerima extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference myRef;
     private String userID;
+    private TextView etNama,etAlamat,etNoTelepon;
 
     private ListView mListViewPenerima;
+    private ImageView ivProfil;
+
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+    //Deklarasi Biodata Penerima
+    private String Barang, Kuantitas, NamaDonatur, Id_Donatur, Lat_Donatur, Lng_Donatur, Id_Penerima, Lat_Penerima, Lng_Penerima, Nama_Penerima, Alamat_Penerima, TeleponPenerima;
+
+    Dialog myDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_penerima);
+
+        myDialog = new Dialog(this);
 
         mListViewPenerima = (ListView) findViewById(R.id.listviewPenerima);
 
@@ -87,6 +114,61 @@ public class ShowPenerima extends AppCompatActivity {
 
     }
 
+    public void ShowPopup(View v) {
+        System.out.println(Id_Penerima);
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.penerma_popup);
+        ivProfil = (ImageView) myDialog.findViewById(R.id.imageViewFotoPenerima);
+        storageRef = storage.getReferenceFromUrl("gs://shafood93.appspot.com");
+        storageRef.child("Penerima/FotoProfil/" + Id_Penerima).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                System.out.println(uri);
+                Glide.with(getApplicationContext()).load(uri).into(ivProfil);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+        etNama = (TextView) myDialog.findViewById(R.id.editTextNamaPenerimaPopup);
+        etAlamat = (TextView) myDialog.findViewById(R.id.editTextAlamatPenerimaPopup);
+        etNoTelepon = (TextView) myDialog.findViewById(R.id.editTextNoTelpPopup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        etNama.setText(Nama_Penerima);
+        etAlamat.setText(Alamat_Penerima);
+        etNoTelepon.setText(TeleponPenerima);
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(ShowPenerima.this, ShowKurir.class);
+                mIntent.putExtra("Barang", Barang);
+                mIntent.putExtra("Kuantitas", Kuantitas);
+                mIntent.putExtra("Id Penerima", Id_Penerima);
+                mIntent.putExtra("Id Donatur", Id_Donatur);
+                mIntent.putExtra("Latitude Penerima", Lat_Penerima);
+                mIntent.putExtra("Longitude Penerima", Lng_Penerima);
+                mIntent.putExtra("Latitude Donatur", Lat_Donatur);
+                mIntent.putExtra("Longitude Donatur", Lng_Donatur);
+                mIntent.putExtra("Nama Donatur", NamaDonatur);
+                mIntent.putExtra("Nama Penerima", Nama_Penerima);
+                startActivity(mIntent);
+            }
+        });
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
+
     private void showData(Map<String, Object> dataSnapshot) {
         final ArrayList<String> Nama = new ArrayList<>();
         for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
@@ -118,12 +200,23 @@ public class ShowPenerima extends AppCompatActivity {
             Map transaksi = (Map) entry.getValue();
             Transaksi.add((String) transaksi.get("transaksi"));
         }
+        ArrayList<String> Alamat = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map alamat = (Map) entry.getValue();
+            Alamat.add((String) alamat.get("alamat"));
+        }
+        ArrayList<String> NoTelepon = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map notelepon = (Map) entry.getValue();
+            NoTelepon.add((String) notelepon.get("nohp"));
+        }
         int i = 0;
         final ArrayList<String> listNama = new ArrayList<>();
         final ArrayList<String> listId = new ArrayList<>();
         final ArrayList<String> listLat = new ArrayList<>();
         final ArrayList<String> listLng = new ArrayList<>();
-        System.out.println(Nama +" | "+ Transaksi + " | "+ Request);
+        final ArrayList<String> listalamat = new ArrayList<>();
+        final ArrayList<String> listtelepon = new ArrayList<>();
         if (Nama != null) {
             while (Nama.size() > i) {
                 if (Request.get(i).equals("true")) {
@@ -132,6 +225,8 @@ public class ShowPenerima extends AppCompatActivity {
                         listId.add(Id_penerima.get(i));
                         listLat.add(Lat.get(i));
                         listLng.add(Lng.get(i));
+                        listalamat.add(Alamat.get(i));
+                        listtelepon.add(NoTelepon.get(i));
                     }
                 }
                 i++;
@@ -144,32 +239,27 @@ public class ShowPenerima extends AppCompatActivity {
                         listId.add(Id_penerima.get(i));
                         listLat.add(Lat.get(i));
                         listLng.add(Lng.get(i));
+                        listalamat.add(Alamat.get(i));
+                        listtelepon.add(NoTelepon.get(i));
                     }
+                    i++;
                 }
-                i++;
             }
-            System.out.println(listNama);
             mListViewPenerima.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    String Barang = getIntent().getStringExtra("Barang");
-                    String Kuantitas = getIntent().getStringExtra("Kuantitas");
-                    String NamaDonatur = getIntent().getStringExtra("Nama Donatur");
-                    String Id_Donatur = getIntent().getStringExtra("Id Donatur");
-                    String Lat_Donatur = getIntent().getStringExtra("Latitude Donatur");
-                    String Lng_Donatur = getIntent().getStringExtra("Longitude Donatur");
-                    toastMessage("Nama = " + listNama.get(position) + " id = " + listId.get(position) + " Barang = " + Barang + " Kuantitas = " + Kuantitas + " Lat Penerima = " + listLat.get(position) + " Lng Penerima = " + listLng.get(position));
-                    Intent mIntent = new Intent(ShowPenerima.this, ShowKurir.class);
-                    mIntent.putExtra("Barang", Barang);
-                    mIntent.putExtra("Kuantitas", Kuantitas);
-                    mIntent.putExtra("Id Penerima", listId.get(position));
-                    mIntent.putExtra("Id Donatur", Id_Donatur);
-                    mIntent.putExtra("Latitude Penerima", listLat.get(position));
-                    mIntent.putExtra("Longitude Penerima", listLng.get(position));
-                    mIntent.putExtra("Latitude Donatur", Lat_Donatur);
-                    mIntent.putExtra("Longitude Donatur", Lng_Donatur);
-                    mIntent.putExtra("Nama Donatur", NamaDonatur);
-                    mIntent.putExtra("Nama Penerima", listNama.get(position));
-                    startActivity(mIntent);
+                    Barang = getIntent().getStringExtra("Barang");
+                    Kuantitas = getIntent().getStringExtra("Kuantitas");
+                    NamaDonatur = getIntent().getStringExtra("Nama Donatur");
+                    Id_Donatur = getIntent().getStringExtra("Id Donatur");
+                    Lat_Donatur = getIntent().getStringExtra("Latitude Donatur");
+                    Lng_Donatur = getIntent().getStringExtra("Longitude Donatur");
+                    Id_Penerima = listId.get(position);
+                    Lat_Penerima = listLat.get(position);
+                    Lng_Penerima = listLng.get(position);
+                    Nama_Penerima = listNama.get(position);
+                    Alamat_Penerima = listalamat.get(position);
+                    TeleponPenerima = listtelepon.get(position);
+                    ShowPopup(view);
 
                 }
             });
