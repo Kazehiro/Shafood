cf8abf5a666e0d6f56c2efcd33f08dd20235199e
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2018-11-13 10:48:40 +0700

    Dwiky Tolol

---
+++ app/src/main/java/com/example/root/shafood/Authors.java
@@ -0,0 +1,13 @@
+package com.example.root.shafood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Authors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authors);
+    }
+}
