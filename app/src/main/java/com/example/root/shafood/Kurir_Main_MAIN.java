01b83cbce4be6e4e45b0e52d2bca20c8149d11bb
dwikyhardi <dwikyhardiansah93@gmail.com>
2018-11-18 19:28:50 +0700

    kurir

--- app/src/main/java/com/example/root/shafood/Kurir_Main_MAIN.java
+++ app/src/main/java/com/example/root/shafood/Kurir_Main_MAIN.java
@@ -1,8 +1,11 @@
 package com.example.root.shafood;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.net.Uri;
@@ -14,9 +17,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,6 +46,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Kurir_Main_MAIN extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
@@ -50,12 +59,13 @@ public class Kurir_Main_MAIN extends AppCompatActivity implements GoogleApiClien
     private LinearLayout mulai;
     private long backPressedTime;
     private Toast backToast;
-    private Button btn_buka_maps, kerja, btn_buka_history, btn_account;
+    private ImageView fotoHistory;
+    private Button btn_buka_maps, kerja, btn_buka_history, btn_account,btnGo;
     private ImageView foto, icon_maps, icon_history, icon_account;
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private DatabaseReference myRef, myRef2;
+    private DatabaseReference myRef,myRef1, myRef2;
     private String userID, namaProfile;
     private FirebaseStorage storage;
     private StorageReference storageRef;
@@ -66,16 +76,19 @@ public class Kurir_Main_MAIN extends AppCompatActivity implements GoogleApiClien
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private FloatingActionButton fab_Logout;
-
-    private static int UPDATE_INTERVAL = 5000;
-    private static int FASTEST_INTERVAL = 3000;
+    private ListView listViewBelumTerkirim;
+    private String NamaPenerima, NamaPengirim, IdPenerima , NamaDonaturPopup, Status;
+    private Dialog buka;
+    private TextView etNamaPenerima, etNamaPengirim, etAlamatPenerima, etNamaDonatur;
+    private static int UPDATE_INTERVAL = 2000;
+    private static int FASTEST_INTERVAL = 2000;
     private static int DISTANCE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kurir_main_main);
-
+        buka = new Dialog(this);
         nama = (TextView) findViewById(R.id.nama);
         check = (ImageView) findViewById(R.id.check);
         mulai = (LinearLayout) findViewById(R.id.mulai);
@@ -84,23 +97,38 @@ public class Kurir_Main_MAIN extends AppCompatActivity implements GoogleApiClien
         icon_maps = (ImageView) findViewById(R.id.icon_maps);
         fab_Logout = (FloatingActionButton) findViewById(R.id.fab_Logout);
         kerja = (Button) findViewById(R.id.kerja);
-        tvNotif = (TextView) findViewById(R.id.tvNotif);
+        listViewBelumTerkirim = (ListView) findViewById(R.id.listViewBelumTerkirim);
         btn_buka_history = (Button) findViewById(R.id.btn_buka_history);
         btn_account = (Button) findViewById(R.id.btn_account);
         icon_account = (ImageView) findViewById(R.id.icon_account);
         icon_history = (ImageView) findViewById(R.id.icon_history);
         jumlahNarik = (TextView) findViewById(R.id.jumlahNarik);
+        
 
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         myRef = mFirebaseDatabase.getReference();
         myRef2 = mFirebaseDatabase.getReference().child("SHAFOOD").child("USER").child("KURIR");
+        myRef1 = mFirebaseDatabase.getReference().child("SHAFOOD").child("TRANSAKSI");
         FirebaseUser user = mAuth.getCurrentUser();
         userID = user.getUid();
         System.out.println("INI UID ======== " + userID);
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReferenceFromUrl("gs://shafood93.appspot.com");
 
+
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                showData1((Map<String, Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        
         btn_buka_maps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -198,6 +226,128 @@ public class Kurir_Main_MAIN extends AppCompatActivity implements GoogleApiClien
             }
         });
 
+    }
+
+    private void showData1(Map<String, Object> dataSnapshot) {
+        final ArrayList<String> Id_Donatur = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_donatur = (Map) entry.getValue();
+            Id_Donatur.add((String) id_donatur.get("id_donatur"));
+        }
+        final ArrayList<String> Id_Kurir = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_kurir = (Map) entry.getValue();
+            Id_Kurir.add((String) id_kurir.get("id_kurir"));
+        }
+        final ArrayList<String> Id_Penerima = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_penerima = (Map) entry.getValue();
+            Id_Penerima.add((String) id_penerima.get("id_penerima"));
+        }
+        final ArrayList<String> Nm_Penerima = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map nm_penerima = (Map) entry.getValue();
+            Nm_Penerima.add((String) nm_penerima.get("nama_penerima"));
+        }
+        final ArrayList<String> Nm_Pengirim = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map nm_pengirim = (Map) entry.getValue();
+            Nm_Pengirim.add((String) nm_pengirim.get("nama_kurir"));
+        }
+        final ArrayList<String> Success = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map success = (Map) entry.getValue();
+            Success.add((String) success.get("success"));
+        }
+        final ArrayList<String> Id_Transaksi = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_transaksi = (Map) entry.getValue();
+            Id_Transaksi.add((String) id_transaksi.get("id_transaksi"));
+        }
+        final ArrayList<String> Nm_Donatur = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map nm_donatur = (Map) entry.getValue();
+            Nm_Donatur.add((String) nm_donatur.get("nama_donatur"));
+        }
+        int i = 0;
+        final ArrayList<String> listId = new ArrayList<>();
+        final ArrayList<String> listIdPenerima = new ArrayList<>();
+        final ArrayList<String> listNamaPenerima = new ArrayList<>();
+        final ArrayList<String> listNamaPengirim = new ArrayList<>();
+        final ArrayList<String> listNamaDonatur = new ArrayList<>();
+
+        if (Id_Kurir != null) {
+            while (Id_Kurir.size() > i) {
+                if (Id_Kurir.get(i).equals(userID)) {
+                    if (Success.get(i).equals("false")) {
+                        listId.add(Id_Transaksi.get(i));
+                        listIdPenerima.add(Id_Penerima.get(i));
+                        listNamaPengirim.add(Nm_Pengirim.get(i));
+                        listNamaDonatur.add(Nm_Donatur.get(i));
+                        listNamaPenerima.add(Nm_Penerima.get(i));
+                    }
+                }
+                i++;
+            }
+            listViewBelumTerkirim.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    IdPenerima = listIdPenerima.get(position);
+                    NamaPenerima = listNamaPenerima.get(position);
+                    NamaPengirim = listNamaPengirim.get(position);
+                    NamaDonaturPopup = listNamaDonatur.get(position);
+                    ShowPopupHistory(view);
+                }
+            });
+            ArrayAdapter namaUser = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listNamaPenerima);
+            listViewBelumTerkirim.setAdapter(namaUser);
+        }
+    }
+
+    public void ShowPopupHistory(View v) {
+        TextView txtclose;
+        buka.setContentView(R.layout.kurir_notif);
+        fotoHistory = (ImageView) buka.findViewById(R.id.imageViewHistoryFotoPenerima);
+        etNamaPenerima = (TextView) buka.findViewById(R.id.editTextHistoryNamaPenerimaPopup);
+        etNamaPengirim = (TextView) buka.findViewById(R.id.editTextHistoryNamaPengirim);
+        etAlamatPenerima = (TextView) buka.findViewById(R.id.editTextHistoryAlamatPenerimaPopup);
+        etNamaDonatur = (TextView) buka.findViewById(R.id.editTextHistoryNamaDonaturPopup);
+        btnGo = (Button) buka.findViewById(R.id.btnGo);
+
+        storageRef.child("Penerima/FotoProfil/" + IdPenerima).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                System.out.println(uri);
+                Glide.with(getApplicationContext()).load(uri).into(fotoHistory);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+        txtclose =(TextView) buka.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        etNamaPenerima.setText("Penerima : " + NamaPenerima);
+        etNamaPengirim.setText("Kurir : " + NamaPengirim);
+        etNamaDonatur.setText("Donatur : "+NamaDonaturPopup);
+        etAlamatPenerima.setText("Status : Belum Terkirim");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buka.dismiss();
+            }
+        });
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(Kurir_Main_MAIN.this,Kurir_Main.class);
+                startActivity(mIntent);
+            }
+        });
+        buka.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        buka.show();
     }
 
     private void showData(DataSnapshot dataSnapshot) {
